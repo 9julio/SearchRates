d31386a6cb546de47991cc34fbf777c2f3726e37
Julio Alberto Fernández Guerrero <jafernandez@paradigmadigital.com>
2021-10-25 11:55:30 +0200

    Initial commit

---
+++ src/main/java/com/jafernandez/searchrates/SearchRatesApplication.java
@@ -0,0 +1,13 @@
+package com.jafernandez.searchrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchRatesApplication.class, args);
+	}
+
+}
