c8f25fb4ba117486dc831f7644e57d5c5141d62f
Julio Alberto Fernández Guerrero <jafernandez@paradigmadigital.com>
2021-10-27 09:31:14 +0200

    Improved tests

--- src/test/java/com/jafernandez/searchrates/services/PriceServiceTests.java
+++ src/test/java/com/jafernandez/searchrates/services/PriceServiceTests.java
@@ -2,12 +2,15 @@ package com.jafernandez.searchrates.services;
 
 import com.jafernandez.searchrates.models.dto.Price;
 import com.jafernandez.searchrates.repositories.PriceRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.text.ParseException;
 import java.util.Arrays;
@@ -16,24 +19,29 @@ import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PriceServiceTests {
 
     @InjectMocks
     PriceServiceImpl priceService;
 
     @Mock
-    PriceRepository priceRepository;
+    PriceRepository priceRepositoryMock;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     public void test_OK() throws ParseException {
 
-        MockitoAnnotations.openMocks(this);
-
-        when(this.priceRepository.findPricesByCriteria(
+        when(this.priceRepositoryMock.findPricesByCriteria(
                 nullable(Date.class),
                 nullable(Integer.class),
                 nullable(Integer.class)
@@ -46,9 +54,7 @@ public class PriceServiceTests {
     @Test
     public void test_EmptyResult() throws ParseException {
 
-        MockitoAnnotations.openMocks(this);
-
-        when(this.priceRepository.findPricesByCriteria(
+        when(this.priceRepositoryMock.findPricesByCriteria(
                 nullable(Date.class),
                 nullable(Integer.class),
                 nullable(Integer.class)
